794d159f07ef345e90abe8ee03f2d7bf06c9dd22
Feras Dawod <feras.da94@gmail.com>
2018-12-30 10:15:05 +0200

    Refactor config files and linting options

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  lintOnSave: true
+  lintOnSave: true,
+  pluginOptions: {
+    electronBuilder: {
+      build: {
+        appId: "com.theawkwardprogrammer.tagmania",
+        win: {
+          icon: '',
+        }
+      },
+      chainWebpackMainProcess: config => {
+        // Chain webpack config for electron main process only
+      },
+      chainWebpackRendererProcess: config => {
+        // Chain webpack config for electron renderer process only
+        // The following example will set IS_ELECTRON to true in your app
+        config.plugin('define').tap(args => {
+          args[0]['IS_ELECTRON'] = true
+          return args
+        })
+      },
+    }
+  }
 }
