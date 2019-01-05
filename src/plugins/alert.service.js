057cdefeefb097cdc7f9ee37ff4adc4933bd25bb
Feras Dawod <feras.da94@gmail.com>
2019-01-05 15:46:29 +0200

    Change iziToast options

--- src/plugins/alert.service.js
+++ src/plugins/alert.service.js
@@ -1,26 +1,41 @@
 import iziToast from 'izitoast';
+import Vue from 'vue';
+
+iziToast.settings({
+    progressBar: false,
+    theme: 'dark',
+    position: 'bottomCenter',
+    pauseOnHover: true,
+});
 
 export default {
     info(data) {
-        if (typeof data === 'string') iziToast.info({ message: data });
+        if (typeof data === 'string')
+            iziToast.info({
+                message: data,
+                backgroundColor: Vue.prototype.$vuetify.theme.info,
+            });
         else iziToast.info(data);
     },
 
     success(message) {
         iziToast.success({
             message,
+            backgroundColor: Vue.prototype.$vuetify.theme.success,
         });
     },
 
     error(message) {
         iziToast.error({
             message,
+            backgroundColor: Vue.prototype.$vuetify.theme.error,
         });
     },
 
     warning(message) {
         iziToast.warning({
             message,
+            backgroundColor: Vue.prototype.$vuetify.theme.warning,
         });
     },
 };
